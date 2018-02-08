60cc73c579acdc395075c12aa341ebfdc3f63fa2
Jean-Baptiste Leprince <jeale726@li12-1.ida.liu.se>
2018-02-08 11:00:20 +0100

    beginning of the database_helper implementation

--- database_helper.py
+++ database_helper.py
@@ -1,4 +1,4 @@
-__author__ = 'Sahand Sadjadee'
+__authors__ = 'Jean-Baptiste Leprince and Francisco Perez'
 import sqlite3
 from flask import g
 
@@ -6,35 +6,50 @@ from flask import g
 
 DATABASE = 'database.db'
 
-
+# connect the database
 def connect_db():
     g.db = sqlite3.connect(DATABASE)
 
+#close the database
 def close_db():
     db = getattr(g, 'db', None)
     if db is not None:
         db.close()
 
-def insert_contact(firstname, familyname, mobile):
+#Insert a new user in the database 
+def insert_user(email,password,token,firstname, familyname, gender,city,country):
     result = []
-
     try:
-        cur = g.db.execute("insert into contact values(?,?,?)", [firstname, familyname, mobile])
+		#prepare statement to insert values in the users table
+        cur = g.db.execute("insert into users values(?,?,?,?,?,?,?,?)", [email,password,token,firstname, familyname, gender,city,country])
         g.db.commit()
         return True
     except:
         return False
 
-def get_contact(firstname, familyname):
+#get all the informations about a user depending of his token or email
+def get_user(email,token):
     result = []
-    cursor = g.db.execute("select * from contact where firstname = ? and familyname = ?", [firstname, familyname])
+    # prepare statement to get values depending of the email and the token
+    cursor = g.db.execute("select * from users where email = ? and token = ?", [email, token])
     rows = cursor.fetchall()
     cursor.close()
+    
     for index in range(len(rows)):
-        result.append({'firstname':rows[index][0], 'familyname':rows[index][1], 'mobile':rows[index][2]})
+        result.append({'email':rows[index][0], 'token':rows[index][2], 'firstname':rows[index][3],'familyname':rows[index][4],'gender':rows[index][5],'city':rows[index][6],'country':rows[index][7]})
     return result
 
-
+#get all the messages about a user depending of his email
+def get_messages(email):
+	result = []
+    # prepare statement to get values depending of the email and the token
+    cursor = g.db.execute("select * from messages where úser_id = ?", [email])
+    rows = cursor.fetchall()
+    cursor.close()
+    # return all messages with the writer and the content of the given email
+    for index in range(len(rows)):
+        result.append({'writer_id':rows[index][2], 'content':rows[index][3]})
+    return result
 
 
 
--- schema.sql
+++ schema.sql
@@ -1,1 +1,20 @@
-create table contact(firstname text, familyname text, mobile text, primary key(mobile)); 
+create table users(
+    email text,
+    password text,
+    token text,
+    firstname text,
+    familyname text,
+    gender text,
+    city text,
+    country text,
+    primary key(email)
+);
+create table messages(
+    id integer,
+    user_id text,
+    writer_id text,
+    content text,
+    FOREIGN KEY (user_id) REFERENCES users(email),
+    FOREIGN KEY (writer_id) REFERENCES users(email),
+    primary key(id)
+);
